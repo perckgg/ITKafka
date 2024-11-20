e4d76a9c1bf0baab3915ae675dc62ca43178d4c5
UK1\MinhNg <minhng@joblogic.com>
2024-11-20 11:42:46 +0700

    [Spark] get selected_df

--- Spark/main.py
+++ Spark/main.py
@@ -1,31 +1,66 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, explode
+from pyspark.sql.functions import col, explode, expr, lit, sum, unix_timestamp
 
 
 # Create a SparkSession
 spark = SparkSession.builder.appName("SystemAnalysis").getOrCreate()
 
-# Stream from Kafka
-df = spark.readStream.format("kafka")\
-    .option("kafka.bootstrap.servers", "localhost:9092")\
-    .option("subscribe", "test")\
-    .option("startingOffsets", "earliest")\
-    .load()
+# # Stream from Kafka
+# df = spark.readStream.format("kafka")\
+#     .option("kafka.bootstrap.servers", "localhost:9092")\
+#     .option("subscribe", "test")\
+#     .option("startingOffsets", "earliest")\
+#     .load()
 
-# json_file_path = "./Kafka/Producer/data.json"
-# df = spark.read.json(json_file_path, multiLine=True)
+json_file_path = "data.json"
+df = spark.read.json(json_file_path, multiLine=True)
 
-# # Select required fields from the DataFrame
-# # boot_time
-# selected_fields_df = df.select(
-#     df.basic_info.computer_name.alias("computer_name"),
-#     df.basic_info.serial_number.alias("serial_number"),
-#     df.basic_info.OS.alias("os"),
-#     df.basic_info.system_model.alias("system_model"),
-#     df.basic_info.system_type.alias("system_type"),
-# )
+exploded_df = df.select(
+    explode(col("physical_disks.disk_list")).alias("disk"))
+total_disk_size_df = exploded_df.select(
+    sum(col("disk.size")).alias("total_disk_size"))
+total_disk_size = total_disk_size_df.collect()[0]["total_disk_size"]
 
-# selected_fields_df.show(truncate=False)
 
-# # Stop the Spark session
-# spark.stop()
+# Select required fields from the DataFrame
+selected_fields_df = df.select(
+    # Assume that "computer_name" is unique
+    df.basic_info.computer_name.alias("bi_computer_name"),
+    df.basic_info.serial_number.alias("bi_serial_number"),
+    df.basic_info.boot_time.alias("bi_boot_time"),
+    df.cpu.uptime.alias("cpu_uptime"),
+    df.cpu.processes.alias("cpu_processes"),
+    df.cpu.ultilization.alias("cpu_ultilization"),
+    df.ram.total.alias("ram_total"),
+    df.ram.used.alias("ram_used"),
+    df.battery.percent.alias("battery_percent"),
+    lit(total_disk_size).alias("total_disk_size"),
+    df.physical_disks.total_memory_used.alias("disk_total_used")
+)
+
+selected_fields_df = selected_fields_df.withColumn(
+    "uptime_seconds",
+    expr("""
+        split(cpu_uptime, ':')[0] * 86400 + -- Days to seconds
+        split(cpu_uptime, ':')[1] * 3600 +  -- Hours to seconds
+        split(cpu_uptime, ':')[2] * 60 +    -- Minutes to seconds
+        split(cpu_uptime, ':')[3]           -- Remaining seconds
+    """).cast("int")
+)
+
+selected_fields_df = selected_fields_df.withColumn(
+    "timestamp",
+    (unix_timestamp(col("bi_boot_time")) + col("uptime_seconds")).cast("timestamp")
+)
+
+selected_fields_df.show(truncate=False, vertical=True)
+
+# # Average CPU Utilization
+# avg_cpu_util = selected_fields_df.groupBy("ComputerName").agg({"CPUUtilization": "avg"})
+# # RAM Usage Summary
+# ram_usage = selected_fields_df.groupBy("ComputerName").agg({"TotalRAM": "max", "UsedRAM": "max", "RAMPercentage": "max"})
+# # Disk Usage and Partition Analysis
+# disk_usage = selected_fields_df.select("ComputerName", "DiskList.size", "TotalDiskMemoryUsed").groupBy("ComputerName").sum("size")
+
+# Stop the Spark session
+spark.stop()
